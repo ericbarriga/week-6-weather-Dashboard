1ecd36c0ee6f75e8df3aa156b2f406ca60b7e51e
ericbarriga <ericbarriga@MacBook-Pro.local>
2021-12-29 17:52:05 -0700

    working api

--- javascrip.js
+++ javascrip.js
@@ -1,18 +1,33 @@
-let srhBtn = document.querySelector('.button');
-let input = document.querySelector('.input');
+let btn = document.querySelector('.button')
+let inputValue = document.querySelector('.input')
+let temp = document.querySelector('.temp')
+let wind = document.querySelector('.wind')
+let humidity = document.querySelector('.humidity')
+let uv = document.querySelector('.uv')
+let cityName = document.querySelector('.cityName')
+let m = moment().format('MM-DD-YYYY')
+console.log(m);
 
 
+btn.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=imperial&appid=c5a75995c194b2ad9d45ac9d28b02699')
+        .then(response => response.json())
+        .then(data => {
+            let tempValue = data['main']['temp'];
+            let windValue = data['wind']['speed'];
+            let humidityValue = data['main']['humidity']
+            let nameValue = data['name']
+            console.log(data);
+
+            temp.innerHTML = tempValue;
+            wind.innerHTML = windValue, 'wind speed';
+            humidity.innerHTML = humidityValue;
+            cityName.innerHTML = nameValue + ` ` + m;
+        })
+
+        .catch(err => alert('city does not exist'))
+})
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={f1f109703421f30f223da30762d78149}')
-
-console.log(fetch);
-
-
-
-// srhBtn.addEventListener('click', doSomething, false);
-
-
-// function doSomething(e) {
-//     console.log('yo');
-// }
+// api.openweathermap.org/data/2.5/weather?q=denver&units=imperial&appid=c5a75995c194b2ad9d45ac9d28b02699/// call for city
+// c5a75995c194b2ad9d45ac9d28b02699// my api keys 
